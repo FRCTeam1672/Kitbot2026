b20b5f17f9f27efc5703020a92dc8ceebd7cee46
Noel <noelsambiju@gmail.com>
2026-02-19 00:36:49 -0500

    Add robot control and autonomous command structure
    - Implemented RobotContainer class to manage subsystems and command bindings.
    - Created Autos class for autonomous command sequences, including example auto.
    - Developed CANDriveSubsystem for controlling robot movement using TalonSRX motors.
    - Established CANFuelSubsystem for managing fuel intake and launching mechanisms with SparkMax motors.
    - Added vendor dependencies for CTRE Phoenix and REV Robotics libraries for motor control.
    - Introduced WPILib new commands for enhanced command-based programming.

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import static frc.robot.Constants.DriverConstants.*;
+import static frc.robot.Constants.FuelConstants.*;
+import frc.robot.commands.Autos;
+import frc.robot.subsystems.CANDriveSubsystem;
+import frc.robot.subsystems.CANFuelSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems
+  private final CANDriveSubsystem driveSubsystem = new CANDriveSubsystem();
+  private final CANFuelSubsystem ballSubsystem = new CANFuelSubsystem();
+
+  // The driver's controller
+  private final CommandPS5Controller driverController = new CommandPS5Controller(
+      DRIVER_CONTROLLER_PORT);
+
+  // The autonomous chooser
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    configureBindings();
+
+    // Set the options to show up in the Dashboard for selecting auto modes. If you
+    // add additional auto modes you can add additional lines here with
+    // autoChooser.addOption
+    autoChooser.setDefaultOption("Autonomous", Autos.exampleAuto(driveSubsystem, ballSubsystem));
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the {@link Trigger#Trigger(java.util.function.BooleanSupplier)}
+   * constructor with an arbitrary predicate, or via the named factories in
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses
+   * for {@link CommandXboxController Xbox}/
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandPS5Controller PS5}
+   * controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+
+    // While the left bumper on driver controller is held, intake Fuel
+    driverController.L1()
+        .whileTrue(ballSubsystem.runEnd(() -> ballSubsystem.intake(), () -> ballSubsystem.stop()));
+ 
+  // While the left trigger is held, store in hopper
+  driverController.L2()
+    .whileTrue(ballSubsystem.storeCommand(0.5));
+
+  // While the right bumper is held: alternate spin-up and launch continuously.
+  // This will repeat: spin for SPIN_UP_SECONDS, then launch for LAUNCH_SECONDS,
+  // until the button is released. When released, stop() will be called.
+  driverController.R1()
+    .whileTrue(ballSubsystem.spinAndLaunchAlternatingCommand(SPIN_UP_SECONDS, LAUNCH_SECONDS));
+    // While the cross button is held on the driver controller, eject fuel back out
+    // the intake
+    driverController.cross()
+        .whileTrue(ballSubsystem.runEnd(() -> ballSubsystem.eject(), () -> ballSubsystem.stop()));
+
+    // Set the default command for the drive subsystem to the command provided by
+    // factory with the values provided by the joystick axes on the driver
+    // controller. The Y axis of the controller is inverted so that pushing the
+    // stick away from you (a negative value) drives the robot forwards (a positive
+    // value). The X-axis is also inverted so a positive value (stick to the right)
+    // results in clockwise rotation (front of the robot turning right). Both axes
+    // are also scaled down so the rotation is more easily controllable.
+    driveSubsystem.setDefaultCommand(
+        driveSubsystem.driveArcade(
+            () -> -driverController.getLeftY() * DRIVE_SCALING,
+            () -> -driverController.getRightX() * ROTATION_SCALING));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return autoChooser.getSelected();
+  }
+}
